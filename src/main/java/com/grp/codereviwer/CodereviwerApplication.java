bc56cd67889e2d4734d9d03bf05f111d87311ba9
Gayatri <patilgayatri906@gmail.com>
2026-04-25 12:25:39 +0530

    Initial COmmit

---
+++ src/main/java/com/grp/codereviwer/CodereviwerApplication.java
@@ -0,0 +1,13 @@
+package com.grp.codereviwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodereviwerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodereviwerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grp/codereviwer/CodereviwerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grp.codereviwer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodereviwerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
